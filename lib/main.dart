6afb93263a1211c17f512e75e00fd1505cd3ed6a
hindinghiko <ok919872i@gmail.com>
2022-05-01 12:54:14 +0900

    bodyに条件バー背景追加

--- lib/main.dart
+++ lib/main.dart
@@ -52,19 +52,77 @@ class _MyHomePageState extends State<MyHomePage> {
               size: 40,
             ),
           ]),
-      body: Container(
-        width: double.infinity,
-        child: TextField(
-          style: TextStyle(
-            fontFamily: 'KaiseiOpti-Bold',
-            fontSize: 20,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: TextField(
+              style: TextStyle(
+                fontFamily: 'KaiseiOpti-Bold',
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              decoration: InputDecoration(
+                hintText: '山名・キーワードで探す',
+              ),
+            ),
           ),
-          decoration: InputDecoration(
-            hintText: '山名・キーワードで探す',
+          Padding(
+            padding: EdgeInsets.only(
+              bottom: 25, //下に２０
+            ),
+            child: Container(
+              color: Colors.green,
+              height: 25,
+            ),
           ),
-        ),
+          Padding(
+            padding:
+                EdgeInsets.symmetric(vertical: 25, horizontal: 20), //上と下に２０
+            child: Container(
+              height: 110,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.horizontal(
+                  right: Radius.circular(20),
+                  left: Radius.circular(20),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+            //上と下に２０
+            child: Container(
+              color: Colors.green,
+              height: 110,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 25), //上と下に２０
+            child: Container(
+              color: Colors.green,
+              height: 110,
+            ),
+          ),
+          Align(
+            alignment: Alignment(1, 1),
+            child: ClipOval(
+              child: Material(
+                color: Colors.green,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.search,
+                    size: 40,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
